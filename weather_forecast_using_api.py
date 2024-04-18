53394faff7b4de585178cc768079c078531d7984
kartikpatil2200 <kartikpatil2200@gmail.com>
2024-04-18 22:25:02 +0530

    Developed weather forecast application using weather open source api

---
+++ weather_forecast_using_api.py
@@ -0,0 +1,25 @@
+import requests
+
+MY_LAT = "18.520430"
+MY_LON = "73.856743"
+WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = "471ab42302e28c8d0dc5d96d1aa45142"
+
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LON,
+    "appid": API_KEY,
+    "cnt": 4,
+}
+
+resp = requests.get(WEATHER_ENDPOINT, params=weather_params)
+resp.raise_for_status()
+weather_data = resp.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700 :
+        will_rain = True
+
+print("Take Umbrella with you while going out" if will_rain else "Happy Day")
